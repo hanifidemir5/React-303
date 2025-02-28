0d81a2bb31280aa8562d64a5fc232e587af0be6a
Hanifi Demir <hanifidemir.sftwr@gmail.com>
2025-02-28 16:25:30 +0300

    To-Do Checkbox İşlemleri

--- src/components/ToDoComponents/ToDoContent/ToDoList/item.js
+++ src/components/ToDoComponents/ToDoContent/ToDoList/item.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useTodo } from "../../../../contexts/ToDoContext";
 
 const ToDoItem = ({ todo }) => {
+  const { toggleTodo } = useTodo();
+
   return (
     <li key={todo.id} className={todo.completed ? "completed" : ""}>
       <div className="view">
-        <input checked={todo.completed} className="toggle" type="checkbox" />
+        <input checked={todo.completed} className="toggle" type="checkbox" onChange={() => toggleTodo(todo.id)} />
         <label>{todo.text}</label>
         <button className="destroy"></button>
       </div>
