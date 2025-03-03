88af0a96aa1e2cd725eaae49b1351c124cc452a5
Hanifi Demir <hanifidemir.sftwr@gmail.com>
2025-03-03 16:05:24 +0300

    build related bug fixes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import LandingPage from "./pages";
 import ToDoApp from "./pages/ToDoApp";
-import StateManagementWithContextApi from "./pages/StateManagement";
+import StateManagement from "./pages/StateManagement";
 import { ToDoProvider } from "./contexts/ToDoContext";
 import SCONPMJS from "./pages/SharingComponentsonNPMJS";
 import UnitTesting from "./pages/UnitTesting";
@@ -11,7 +11,7 @@ const App = () => (
     <Routes>
       <Route path="/">
         <Route index element={<LandingPage />} />
-        <Route element={<StateManagementWithContextApi />} path="/SMWCAPI" />
+        <Route element={<StateManagement />} path="/SMWCAPI" />
         <Route
           element={
             <ToDoProvider>
